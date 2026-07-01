fd59ea1417bc835cf3b3b6c6af52f736c243784d
Richard Yvarra <richard.yvarra@gmail.com>
2026-07-01 19:55:48 -0400

    added styling and formatting to the data products page

--- src/components/Visualizations.jsx
+++ src/components/Visualizations.jsx
@@ -1,16 +1,20 @@
 const Visualizations = () => {
   return (
-    <section 
-      id='viztab' 
-      className="content" 
-      style={{ 
-        marginTop: '3rem', 
-        marginBottom: '1.5rem', 
-        marginLeft: '1rem', 
-        marginRight: '1.5rem' 
+    <section
+      id='viztab'
+      className='content'
+      style={{
+        marginTop: '3rem',
+        marginBottom: '1.5rem',
+        marginLeft: '1rem',
+        marginRight: '1.5rem',
+        minHeight: '60vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
       }}
     >
-      <h1>Coming Soon</h1>
+      <h1 style={{ fontSize: '3rem', textAlign: 'center' }}>Coming Soon</h1>
     </section>
   );
 };
